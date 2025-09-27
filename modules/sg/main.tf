d62f2ade96a75d0838b919d4561a445dc8267681
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-27 20:40:20 +0300

    Update main.tf

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -17,7 +17,18 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = { Name = var.name }
+ resource "aws_security_group" "this" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
+
+  # חוקים... (ingress/egress)
+
+  tags = merge(
+    { Name = var.name },
+    var.tags
+  )
+}
 }
 
 
