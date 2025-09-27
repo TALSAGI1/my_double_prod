f8eaa0204d326c03a64fc93c92c46451343224d2
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-27 20:34:35 +0300

    Update providers.tf

--- prod/providers.tf
+++ prod/providers.tf
@@ -1,10 +1,21 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    aws        = { source = "hashicorp/aws",        version = "~> 5.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.29" }
-    helm       = { source = "hashicorp/helm",       version = "~> 2.13" }
-  }
-  # אם יש לך backend ל-state ב-bootstrap, השאירי אותו שם
+provider "aws" {
+  region = var.region
 }
 
+# אם את מתחברת ל-EKS כדי להשתמש ב-kubernetes/helm – הגדירי אותם כאן (ללא terraform{}):
+# data "aws_eks_cluster" "this" { name = module.eks.cluster_name }
+# data "aws_eks_cluster_auth" "this" { name = module.eks.cluster_name }
+#
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.this.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.this.token
+# }
+#
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.this.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.this.token
+#   }
+# }
