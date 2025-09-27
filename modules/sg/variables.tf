b7213ca8a8591d976b1522ed03eea371692275f9
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-27 20:50:30 +0300

    Update variables.tf

--- modules/sg/variables.tf
+++ modules/sg/variables.tf
@@ -1,3 +1,18 @@
-variable "vpc_id" { type = string }
-variable "name" { type = string }
-variable "allowed_ports" { type = list(number) }
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type    = string
+  default = "Allow inbound traffic"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the security group"
+  type        = map(string)
+  default     = {}
+}
