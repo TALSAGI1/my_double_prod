7ba91ab04258d414984e9a87582b65c38d569016
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-27 19:21:23 +0300

    Create providers.tf

---
+++ prod/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws        = { source = "hashicorp/aws",        version = "~> 5.0" }
+    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.29" }
+    helm       = { source = "hashicorp/helm",       version = "~> 2.13" }
+  }
+  # אם יש לך backend ל-state ב-bootstrap, השאירי אותו שם
+}
+
+provider "aws" {
+  region = var.region
+}
