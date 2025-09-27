f38f92f4c80a171d2f6a67953238a46e21fe8806
TALSAGI1 <139162010+TALSAGI1@users.noreply.github.com>
2025-09-27 20:56:58 +0300

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,3 +2,8 @@ variable "cidr" { type = string }
 variable "name" { type = string }
 variable "public_subnets" { type = list(string) }
 variable "azs" { type = list(string) }
+variable "private_subnets"  { type = list(string) }
+variable "enable_nat_gateway" { type = bool, default = true }
+variable "single_nat_gateway" { type = bool, default = true }
+variable "enable_nat_gateway" { type = bool, default = true }
+variable "single_nat_gateway" { type = bool, default = true }
